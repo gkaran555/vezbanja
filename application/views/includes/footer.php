83fc99dd85360c9fca5af4adbff4ed0a94bc806d
gkaran555 <gkaran555@gmail.com>
2016-06-08 18:57:38 +0200

    foo

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -2,12 +2,12 @@
 <footer  class="footer">
   
   <div class="container text-center">
-    <a href="https://www.facebook.com/goran.karan.10"><i class="fa fa-facebook"></i></a>
-	<a href="https://plus.google.com/118417246482862346589/photos"><i class="fa fa-google-plus"></i></a>
+    <a href="https://www.facebook.com/goran.karan.10" target="_blank"><i class="fa fa-facebook"></i></a>
+	<a href="https://plus.google.com/118417246482862346589/photos" target="_blank"><i class="fa fa-google-plus"></i></a>
 	<a href="#"><i class="fa fa-youtube"></i></a>
     <a href="#"><i class="fa fa-twitter"></i></a>
-    <a href="https://www.linkedin.com/in/g-karan-05331395?trk=nav_responsive_tab_profile"><i class="fa fa-linkedin"></i></a>
-    <a href="https://github.com/gkaran555"><i class="fa fa-github-square"></i></a>
+    <a href="https://rs.linkedin.com/in/goran-karan-05331395?trk=prof-samename-name" target="_blank"><i class="fa fa-linkedin"></i></a>
+    <a href="https://github.com/gkaran555" target="_blank"><i class="fa fa-github-square"></i></a>
   </div>
      
 </footer>
